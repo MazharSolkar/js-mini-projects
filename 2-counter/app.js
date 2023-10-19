22552a47b5a7603297e7aadd7917f7702517da88
MazharSolkar <mazharsolkar65@gmail.com>
2023-10-19 21:42:48 +0530

    updated

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -1,22 +1,22 @@
 const countSpan = document.querySelector('#count-span');
 const btnContainer = document.querySelector('.btn-container');
+
 let count = 0;
 
-function setCount(newCount) {
+const setCount = (newCount) => {
 	count = newCount;
 	countSpan.innerHTML = count;
 
-	countSpan.classList.toggle('text-green-500', count > 0);
 	countSpan.classList.toggle('text-red-500', count < 0);
-	countSpan.classList.toggle('text-black', count === 0);
-}
+	countSpan.classList.toggle('text-green-500', count > 0);
+};
 
 btnContainer.addEventListener('click', (e) => {
 	if (e.target.id === 'decrease') {
 		setCount(count - 1);
 	} else if (e.target.id === 'increase') {
 		setCount(count + 1);
-	} else {
+	} else if (e.target.id === 'reset') {
 		setCount(0);
 	}
 });
