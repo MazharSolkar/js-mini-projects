876f593e4e16e7613603ee2da915e5ec1d6908c4
MazharSolkar <mazharsolkar65@gmail.com>
2023-10-24 16:20:21 +0530

    es-6 filter ✅

---
+++ 9-filters/app.js
@@ -0,0 +1,82 @@
+import { products } from './products.js';
+
+const searchBtns = document.querySelector('#search-btns');
+console.log(searchBtns);
+
+const setSearchBtns = () => {
+	const btns = ['all', ...new Set(products.map((product) => product.company))];
+	console.log(btns);
+
+	searchBtns.innerHTML = btns
+		.map(
+			(btn) => `
+  <button class="btn block text-gray-500 hover:text-gray-900 mb-2" data-id="${btn}">
+    ${btn}
+  </button>`
+		)
+		.join('');
+};
+
+setSearchBtns();
+
+let filteredProducts = [...products];
+// console.log(filteredProducts);
+
+const productsContainer = document.querySelector('#products-container');
+
+const setProductsContainer = () => {
+	// when none of the products title matches the search string.
+	if (filteredProducts.length < 1) {
+		productsContainer.innerHTML = `<h1 class="text-3xl font-bold text-red-500">Sorry, no products matched your search 😞</h1>`;
+		return;
+	}
+	//
+	productsContainer.innerHTML = filteredProducts
+		.map(
+			(product) =>
+				`
+    <article class="w-[500px] lg:w-full rounded-lg shadow-lg text-center">
+      <img
+          class="h-[250px] w-full object-cover rounded-lg"
+          src="${product.image}"
+          alt="${product.title}" />
+      <h1 id="title" class="py-2 text-gray-500">${product.title}</h1>
+      <p class="text-gray-700 font-semibold">₹ ${product.price}</p>
+  </article>`
+		)
+		.join('');
+};
+
+setProductsContainer();
+
+const searchInput = document.querySelector('#search');
+
+searchInput.addEventListener('keyup', (e) => {
+	const inputValue = e.target.value.toLowerCase();
+	if (inputValue === '') {
+		filteredProducts = [...products];
+	} else {
+		filteredProducts = products.filter((product) =>
+			product.title.toLowerCase().includes(inputValue)
+		);
+	}
+	setProductsContainer();
+});
+
+searchBtns.addEventListener('click', (e) => {
+	const clickedBtn = e.target;
+
+	if (clickedBtn.tagName !== 'BUTTON') return;
+
+	console.log(clickedBtn.textContent);
+
+	if (clickedBtn.dataset.id === 'all') {
+		filteredProducts = [...products];
+	} else {
+		filteredProducts = products.filter(
+			(product) => product.company === clickedBtn.dataset.id
+		);
+		console.log(filteredProducts);
+	}
+	setProductsContainer();
+});
---
+++ 9-filters/products.js
@@ -0,0 +1,88 @@
+export const products = [
+	{
+		id: 'rec43w3ipXvP28vog',
+		title: 'high-back bench',
+		company: 'ikea',
+		image: 'https://course-api.com/images/store/product-1.jpeg',
+		price: 2000,
+	},
+	{
+		id: 'rec4f2RIftFCb7aHh',
+		title: 'albany table',
+		company: 'marcos',
+		image: 'https://course-api.com/images/store/product-2.jpeg',
+		price: 4000,
+	},
+	{
+		id: 'rec8kkCmSiMkbkiko',
+		title: 'accent chair',
+		company: 'caressa',
+		image: 'https://course-api.com/images/store/product-3.jpeg',
+		price: 2500,
+	},
+	{
+		id: 'recBohCqQsot4Q4II',
+		title: 'wooden table',
+		company: 'caressa',
+		image: 'https://course-api.com/images/store/product-4.jpeg',
+
+		price: 4500,
+	},
+	{
+		id: 'recDG1JRZnbpRHpoy',
+		title: 'dining table',
+		company: 'caressa',
+		image: 'https://course-api.com/images/store/product-5.jpeg',
+
+		price: 6999,
+	},
+	{
+		id: 'recNWGyP7kjFhSqw3',
+		title: 'sofa set',
+		company: 'liddy',
+		image: 'https://course-api.com/images/store/product-6.jpeg',
+		price: 16999,
+	},
+	{
+		id: 'recZEougL5bbY4AEx',
+		title: 'modern bookshelf',
+		company: 'marcos',
+		image: 'https://course-api.com/images/store/product-7.jpeg',
+		price: 1899,
+	},
+	{
+		id: 'recjMK1jgTb2ld7sv',
+		title: 'emperor bed',
+		company: 'liddy',
+		image: 'https://course-api.com/images/store/product-8.jpeg',
+		price: 22199,
+	},
+	{
+		id: 'recmg2a1ctaEJNZhu',
+		title: 'utopia sofa',
+		company: 'marcos',
+		image: 'https://course-api.com/images/store/product-9.jpeg',
+		price: 3995,
+	},
+	{
+		id: 'recvKMNR3YFw0bEt3',
+		title: 'entertainment center',
+		company: 'liddy',
+		image: 'https://course-api.com/images/store/product-10.jpeg',
+		price: 2998,
+	},
+	{
+		id: 'recxaXFy5IW539sgM',
+		title: 'albany sectional',
+		company: 'ikea',
+		image: 'https://course-api.com/images/store/product-11.jpeg',
+		price: 1099,
+	},
+	{
+		id: 'recyqtRglGNGtO4Q5',
+		title: 'leather sofa',
+		company: 'liddy',
+		image: 'https://course-api.com/images/store/product-12.jpeg',
+		price: 9999,
+	},
+];
