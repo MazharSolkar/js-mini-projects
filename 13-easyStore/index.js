5ddaa96987b7927acb92d5a6fbd2c7d9e67df569
MazharSolkar <mazharsolkar65@gmail.com>
2023-10-29 11:57:38 +0530

    added info and add cart btn ui

--- 13-easyStore/index.js
+++ 13-easyStore/index.js
@@ -21,20 +21,26 @@ const initialLoad = async () => {
 	// console.log(featuredProducts);
 
 	const featuredProductsContainer = getElement('.featured-products-container');
-	featuredProductsContainer.innerHTML = featuredProducts.map((product) => {
-		const { id, name, image, price } = product;
-		return `
+	featuredProductsContainer.innerHTML = featuredProducts
+		.map((product) => {
+			const { id, name, image, price } = product;
+			return `
 
-	  <article class="single-product rounded-lg shadow-lg border-4 border-black w-[350px] m-4">
+	  <article class="single-product rounded-lg shadow-lg border-4 border-black w-[350px] m-4 relative group">
         <img
         class="object-cover h-[200px] w-full"
         src="${image}"
         alt="" />
+				<div class="btn-container absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+					<button class="info-btn bg-orange-400 text-white rounded-full p-2 mr-2 hidden group-hover:inline-block"><i class="fa-solid fa-circle-info fa-fw"></i></button>
+					<button data-id="${id}" class="info-btn bg-orange-400 text-white rounded-full p-2 hidden group-hover:inline-block"><i class="fa-solid fa-cart-plus fa-fw"></i></button>
+				</div>
 	    <h1 class="product-title text-gray-500 my-1">${name}</h1>
 	    <p class="product-price text-gray-700 font-bold">₹ ${price * 10}</p>
 	  </article>
 	  `;
-	});
+		})
+		.join('');
 };
 
 window.addEventListener('DOMContentLoaded', initialLoad);
--- 13-easyStore/src/pages/products.js
+++ 13-easyStore/src/pages/products.js
@@ -1,1 +1,24 @@
 import '../toggleNavAndSidebar.js';
+import { getElement } from '../utils.js';
+import { allProducts } from '../addAllProductsToLocalStorage.js';
+
+const productsContainer = getElement('.products-container');
+productsContainer.innerHTML = allProducts
+	.map((product) => {
+		const { image, name, id, price } = product;
+		return `
+  <!-- SINGLE PRODUCT -->
+  <article class="single-product rounded-lg shadow-lg border-4 border-black w-[350px] m-4 relative group">
+  <img
+  class="object-cover h-[200px] w-full"
+  src="${image}"
+  alt="" />
+          <div class="btn-container absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+              <button class="info-btn bg-orange-400 text-white rounded-full p-2 mr-2 hidden group-hover:inline-block"><i class="fa-solid fa-circle-info fa-fw"></i></button>
+              <button data-id="${id}" class="info-btn bg-orange-400 text-white rounded-full p-2 hidden group-hover:inline-block"><i class="fa-solid fa-cart-plus fa-fw"></i></button>
+          </div>
+  <h1 class="product-title text-gray-500 my-1">${name}</h1>
+  <p class="product-price text-gray-700 font-bold">₹ ${price * 10}</p>
+</article>`;
+	})
+	.join('');
